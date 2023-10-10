ec480a4bb99059ddbaccc7ebe1c8dbf87eaaac16
laserbeast <mpmarkgraf@gmail.com>
2023-10-10 16:32:49 -0400

    Updated email message

--- src/utilities/constants.tsx
+++ src/utilities/constants.tsx
@@ -1,7 +1,7 @@
 const email = 'christopherMarkgraf@gmail.com';
 const subject = 'Custom%20artwork';
 const message =
-    'Hi%20Christopher,%0D%0AI%27m%20interested%20in%20commissioning%20artwork.%20Can%20you%20please%20provide%20me%20with%20details%20about%20the%20process%2C%20pricing%2C%20and%20timeline%3F%20Thanks%21';
+    'Hi%20Christopher,%0D%0A,%0D%0AI%27m%20interested%20in%20commissioning%20artwork.%20Can%20you%20please%20provide%20me%20with%20details%20about%20the%20process%2C%20pricing%2C%20and%20timeline%3F,%0D%0A,%0D%0AThanks%21';
 
 export const facebookLink = 'https://facebook.com/christopher.markgraf/';
 export const instagramLink = 'https://instagram.com/christopher.markgraf/';
